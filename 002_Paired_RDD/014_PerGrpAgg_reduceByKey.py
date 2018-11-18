146fb4a2f8352fadb79c64ac0519c8654c963afb
herin <herin.learning@users.noreply.github.com>
2018-11-17 23:03:57 -0500

    Reference Material

--- 002_Paired_RDD/014_PerGrpAgg_reduceByKey.py
+++ 002_Paired_RDD/014_PerGrpAgg_reduceByKey.py
@@ -13,27 +13,23 @@ conf = SparkConf().setAppName("014_PerGrpAgg_reduceByKey").setMaster(sys.argv[1]
 sc = SparkContext(conf = conf)
 
 input_file = sys.argv[2]
-
 orderItems = sc.textFile(input_file)
 
+
 orderItemsMap = orderItems.map(lambda oi: ( int(oi.split(",")[1]),float(oi.split(",")[4]) ))
-
 revenuePerOrderId = orderItemsMap.reduceByKey(lambda x,y : x + y)
-
 for i in revenuePerOrderId.take(10):
     print i
 
 
+#Alternate way to perform reduceByKey using add operator from operator library
 revenuePerOrderId = orderItemsMap.reduceByKey(add)
-
 for i in revenuePerOrderId.take(10):
     print i
 
 
 # Get minimum revenue per Order Id
-
 minRevenuePerOrderId = orderItemsMap.reduceByKey(lambda x,y: x if(x<y) else y)
-
 for i in minRevenuePerOrderId.take(10):
     print i
 
